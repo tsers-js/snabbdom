a291e7cbdd0c0178e53317acf776f31f67c5e789
Matti Lankinen <m.lankinen@iki.fi>
2016-04-13 23:35:28 +0300

    Do not add “data-“ attribute to DOM

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const htmlAttrs =
   "accept accept-charset accesskey action align alt async autocomplete autofocus " +
   "autoplay autosave bgcolor buffered challenge charset checked cite code " +
   "codebase color cols colspan content contenteditable contextmenu controls coords " +
-  "data data- datetime default defer dir dirname disabled download draggable dropzone " +
+  "data datetime default defer dir dirname disabled download draggable dropzone " +
   "enctype for form formaction headers height hidden high href hreflang http-equiv " +
   "icon id ismap itemprop keytype kind label lang language list loop low manifest " +
   "max maxlength media method min multiple muted name novalidate open optimum pattern " +
